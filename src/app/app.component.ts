24fde01a9221cdbeee87833c9de02dec9edac0d9
victoriadaluz <vicky.daluz@gmail.com>
2025-04-22 11:56:07 -0300

    first commit: carrousel component, assets, navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CarrouselComponent } from './carrousel/carrousel.component';
 @Component({
+  /*decorador afecta a la clase appcomponent. es un patrón decoradorx */
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CarrouselComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tivoli-front-end';
