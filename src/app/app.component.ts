52da3310f81d9722389c2c392233b12d4e977d8e
victoriadaluz <vicky.daluz@gmail.com>
2025-04-30 16:48:03 -0300

    desplegables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CarrouselComponent } from './carrousel/carrousel.component';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 @Component({
   /*decorador afecta a la clase appcomponent. es un patrón decoradorx */
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CarrouselComponent],
+  imports: [RouterOutlet, CarrouselComponent, MatExpansionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tivoli-front-end';
+  readonly panelOpenState = signal(false);
 }
