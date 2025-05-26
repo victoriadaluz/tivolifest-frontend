7588b1f90d3db0b20f82248d068252c4748fc902
victoriadaluz <vicky.daluz@gmail.com>
2025-05-26 11:28:25 -0300

    responsive styles and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { RouterOutlet } from '@angular/router';
 import { CarrouselComponent } from './carrousel/carrousel.component';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { IonIcon } from '@ionic/angular/standalone';
 @Component({
   /*decorador afecta a la clase appcomponent. es un patrón decoradorx */
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CarrouselComponent, MatExpansionModule],
+  imports: [RouterOutlet, CarrouselComponent, MatExpansionModule, IonIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
