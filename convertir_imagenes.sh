d0fb1a02189be6b91869eaf01d6e19d76407acce
victoriadaluz <vicky.daluz@gmail.com>
2025-06-06 14:15:36 -0300

    deleted all jpg files and replaced with webp files

--- convertir_imagenes.sh
+++ convertir_imagenes.sh
@@ -1,5 +1,5 @@
 # Ruta base ajustada para Windows/Git Bash
-CARPETA_IMAGENES="src/assets"
+CARPETA_IMAGENES="src/assets/fichines"
 
 # Busca todas las carpetas y convierte imágenes
 find "$CARPETA_IMAGENES" -type d | while read -r carpeta; do
@@ -7,7 +7,7 @@ find "$CARPETA_IMAGENES" -type d | while read -r carpeta; do
     cd "$carpeta" || exit
     
     # Convierte imágenes a WebP (compatible con espacios en nombres)
-    for img in *.jpg *.JPG *.png *.PNG; do
+    for img in *.jpg *.JPG *.png *.PNG *HEIC; do
         if [ -f "$img" ]; then
             echo "Convirtiendo $img a WebP..."
             cwebp -q 85 "$img" -o "${img%.*}.webp"
